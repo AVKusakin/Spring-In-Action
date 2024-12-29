26f593a81d1ed9f3a141c16184bcceb23f16efa9
AVKusakin <avkusakin1995@gmail.com>
2024-12-29 11:24:39 +0500

    adjustments

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.springinaction;
+package tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringInActionApplicationTests {
+class TacoCloudApplicationTests {
 
     @Test
     void contextLoads() {
